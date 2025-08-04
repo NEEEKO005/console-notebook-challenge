872c8a789169492ba860bc2f6f9bc4c3fe36190b
nicolas correa <nicolascorreapulido@gmail.com>
2025-08-04 17:40:22 -0500

    Agregar inicialisacion de variables y date time

--- src/notebook.py
+++ src/notebook.py
@@ -1,8 +1,8 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
 
 from datetime import datetime
+from typing import List
 
-datetime.now()
 
 class Note:
 
@@ -12,8 +12,9 @@ class Note:
 
 
     def __init__(self,code,title,text,importance):
-        self.code = code
-        self.title = title
-        self.text = text
-        self.importance = importance
+        self.code:int = code
+        self.title:str = title
+        self.text:str = text
+        self.importance:str = importance
         self.creation_date = datetime.now()
+        self.tags: List[str] = []
