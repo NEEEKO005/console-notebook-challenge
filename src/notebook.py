42ad7e57ba35c978846f44dd5264d03e74173643
nicolas correa <nicolascorreapulido@gmail.com>
2025-08-04 17:47:07 -0500

    Creacion de funciones add tag

--- src/notebook.py
+++ src/notebook.py
@@ -1,20 +1,22 @@
-# TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
-
 from datetime import datetime
 from typing import List
 
-
 class Note:
+    # Constantes de clase
+    HIGH: str = 'HIGH'
+    MEDIUM: str = 'MEDIUM'
+    LOW: str = 'LOW'
 
-    HIGH: str="High"
-    MEDIUM: str = "Medium"
-    LOW: str="Low"
-
-
-    def __init__(self,code,title,text,importance):
-        self.code:int = code
-        self.title:str = title
-        self.text:str = text
-        self.importance:str = importance
-        self.creation_date = datetime.now()
+    def __init__(self, code: str, title: str, text: str, importance: str):
+        self.code: str = code
+        self.title: str = title
+        self.text: str = text
+        self.importance: str = importance
+        self.creation_date: datetime = datetime.now()
         self.tags: List[str] = []
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    
