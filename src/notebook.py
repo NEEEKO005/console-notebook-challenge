fc068853b0295f2973cc969396fdeaf6ac34c810
nicolas correa <nicolascorreapulido@gmail.com>
2025-08-04 17:20:30 -0500

    Inicio de proyecto, creacion de clase Note

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,19 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+
+from datetime import datetime
+
+datetime.now()
+
+class Note:
+
+    HIGH: str="High"
+    MEDIUM: str = "Medium"
+    LOW: str="Low"
+
+
+    def __init__(self,code,title,text,importance):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+        self.creation_date = datetime.now()
