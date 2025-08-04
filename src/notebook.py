4a8e89ac3754639778ac52e2f7be3b5733cbeb05
nicolas correa <nicolascorreapulido@gmail.com>
2025-08-04 17:49:04 -0500

    Creacion de la nueva clase Notebook

--- src/notebook.py
+++ src/notebook.py
@@ -19,4 +19,19 @@ class Note:
         if tag not in self.tags:
             self.tags.append(tag)
 
-    
+
+class Notebook:
+
+    def __init__(self):
+        self.notes: List[Note] = []
+
+    def add_note(self, title: str, text: str, importance: str) -> int:
+        new_code = 1
+        existing_codes = {int(note.code) for note in self.notes}
+        while new_code in existing_codes:
+            new_code += 1
+
+        new_note = Note(str(new_code), title, text, importance)
+        self.notes.append(new_note)
+
+        return new_code
